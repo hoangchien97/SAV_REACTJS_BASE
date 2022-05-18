8053057e63dade9b2076a3ed6722d204c7737dab
Chien Hoang <chien.hoang@savvycomsotware.com>
2022-05-18 10:15:58 +0700

    implement locales for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { Suspense } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Header } from "components/Layout/Header/Header";
+import { useTranslation } from "react-i18next";
+import "i18n/config";
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <img src={logo} className="App-logo" alt="logo" />
+    <div>loading...</div>
+  </div>
+);
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className="App">
+        <Header />
+        <div>{t("title", { framework: "React" })}</div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <button type="button" onClick={() => changeLanguage("de")}>
+            de
+          </button>
+          <button type="button" onClick={() => changeLanguage("en")}>
+            en
+          </button>
+        </header>
+      </div>
+    </Suspense>
   );
 }
 
